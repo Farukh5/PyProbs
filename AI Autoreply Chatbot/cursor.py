20e5fdc0fc5cbb13197aa1ac614ad47cf6a5a4b9
Farukh5 <choudhary.m.farukh@gmail.com>
2024-07-24 12:21:48 +0500

    Update cursor.py

--- AI Autoreply Chatbot/cursor.py
+++ AI Autoreply Chatbot/cursor.py
@@ -1,1 +1,12 @@
+import pyautogui
 
+while True:
+    a = pyautogui.position()
+    print(a)
+
+    #You can change coordinates according to your choice.
+
+    # 1639, 1412
+    # 1003 237 to 2187 1258
+    # 1026 244
+    # to 1131 1321
